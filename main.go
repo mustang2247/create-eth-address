ceb46b3a601e9e746da7e68e3700aff897ed2c6d
mustangkong <mustang2247@gmail.com>
2018-07-04 21:23:41 +0800

    init import

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"flag"
+	"io"
+	"os"
+)
+
+var key *keystore.KeyStore
+var password *string
+
+func main()  {
+
+	password = flag.String("pw", "123456", "")
+	flag.Parse()
+
+	fmt.Println("pw:  " + *password)
+	key = keystore.NewKeyStore("./keystores", keystore.StandardScryptN, keystore.LightScryptP)
+	for i := 0; i < 200; i++  {
+		newAccount()
+	}
+
+}
+
+func newAccount()  {
+	//key := keystore.NewKeyStore("./keystores", keystore.StandardScryptN, keystore.LightScryptP)
+	act, err := key.NewAccount(*password)
+	if err != nil {
+		fmt.Println(err)
+	}else {
+		fd,_ := os.OpenFile("./keystores/addr.txt",os.O_RDWR|os.O_CREATE|os.O_APPEND,0644)
+		s := fmt.Sprintf("%s\n", act.Address.Hex())
+		io.WriteString(fd,s)
+
+		fmt.Println(act.Address.Hex())
+	}
+}
+
+
